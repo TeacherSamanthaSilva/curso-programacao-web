d2b800edd4f605511bf2d597ba99e6aebc196257
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2020-12-10 23:36:31 -0300

    criação do arquivo index
    Neste commit foi criado o arquivo index do projeto

---
+++ site-curso-programacaoweb/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Biblos</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="materialize/css/materialize.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap" rel="stylesheet">
+   
+  </head>
+    <body>
+     
+      <h1 class=" ndigo-text.text-lighten-3" class="z-depth-4 class="flow-text">Biblos a Bíblia Gamificada</h1>
+     
+      <section id="conhecabiblos">
+        <h2 class="flow-text">O que você precisa saber antes de baixar o Biblos</h2>
+      <p class="flow-text">Desmotivado para ler a Bíblia? Seus problemas acabaram. Pois com o Biblos você vai ter a experiência de ler a Bìblia como se estivesse num jogo. É isso mesmo que você está lendo, pois o Biblos através da gamificação transformou os livros em níveis e os capítulos em fases, transformando a Bíblia em um jogo. Baixe o Biblos agora mesmo e estude a Bíblia como se estivesse num jogo</p>
+      <a href="">Clique aqui para baixar o Biblos</a>
+      <div class="imagem"  ><img src="foto1.jpg" class="parallax opacidade" class="responsive-img" alt="jovem com celular na mão" width="500" height="500"></div>
+      </section>
+      <section id = "oqueeobiblos">
+      
+
+        <h3 class="flow-text">O que é o Biblos</h3>
+        
+      
+            <p class="flow-text">Este projeto detalha o desenvolvimento de um jogo para dispositivos móveis batizado de Biblos a Bíblia gamificada, trata-se de uma nova tecnologia criada para ajudar pessoas de todas as idades a desenvolver o hábito da leitura diária da Bíblia. Biblos a bíblia gamificada é um jogo para celulares e tablets criado para dar aqueles que desejam evoluir nos seus estudos bíblicos a motivação necessária para desenvolver o hábito da leitura diária da Bíblia usando como principal ferramenta a gamificação, e assim ajuda-los a ter um melhor entendimento do conteúdo deste livro e crescimento espiritual, tudo isso de uma forma simples e divertida. </p>
+          
+        
+        
+        
+
+           <p class="flow-text">O jogo Biblos segue o cânon utilizado pelas bíblias protestantes, que é formado pelos 39 livros do antigo testamento e pelos 27 do novo testamento. Porem em vez do começar pelo livro de Genesis optou-se por começar pelo livro de Filemon, por este ser um dos livros da bíblia com menor quantidade de capítulos. A ideia é começar pelo livro de menor quantidade de capítulos e versículos que é Filemon e terminar no maior livro no quesito quantidade de capítulos e versículos que é Salmos. </p>
+
+      
+
+      
+           <p class="flow-text"> A Paráfrase que foi utilizada no desenvolvimento do aplicativo Biblos, a bíblia gamificada foi à Nova Almeida Atualizada desenvolvida e lançada pela Sociedade Bíblica do Brasil em 2017. Pelo fato desta tradução usar uma linguagem de mais fácil entendimento pelo nosso público alvo ser uma das paráfrases mais populares no meio cristão e mais próxima da linguagem coloquial.  </p>
+
+        <p class="flow-text">Para o desenvolvimento do Biblos foi utilizado a engine Unity, A IDE Visual Studio e a linguagem de programação C#.</p>
+      
+
+        
+
+       
+       
+
+        <div class="imagem" "><img src="foto2.jpg"class="parallax opacidade" class="responsive-img" alt="Bíblias JesusCopy" width="500" height="500"></div>
+
+    
+
+      </section>
+      <section id ="comojogar">
+        <h3 class="comojogar" class="flow-text">Como Jogar</h3>
+
+        <p class="flow-text">Após fazer o download e a instalação do Biblos, basta clicar na logo para iniciar a sua jornada.Na tela de início do aplicativo você verá as opções de jogar, sair e conheça o Biblos. Clicando jogar você verá a tela de inicio da fase, basta clicar em jogar para ler o texto bíblico, após ler o texto bíblico você será direcionado a tela de pergunta-desafio, se você clicar na resposta correta irá para a próxima fase, senão irá para a tela de inicio da fase e começara tudo de novo. O Objetivo do Biblos e chegar na ultima fase onde você terá lido toda a Bíblia</p>
+       <div class="video-container">
+        <iframe width="853" height="480" src="video.mp4" frameborder="0" allowfullscreen></iframe>
+      </div>
+        
+
+        
+
+      </section>
+      <section id ="quemsoueu">
+        <h3 class="flow-text">Quem sou eu</h3>
+        <p class="flow-text">Meu nome é Samantha Silva tenho 26 anos sou estudante de Ciência da Computação na Unifacvest e Membra da Igreja Adventista do sétimo dia, Criei o Biblos com o intuito de mudar totalmenta a forma como as pessoas veem a leitura bíblica, espero que com o  Biblos ler a Bíblia se torne algo divertido</p>
+        <div class="imagem"  "><img src="foto3.jpg" class="parallax opacidade" class="responsive-img" alt="Bíblias JesusCopy" width="500" height="500"></div>
+        <p> Para saber mais sobre mim basta acessar minhas redes sociais</p>
+        <ul>
+        <li><a  target="_blank "href="https://www.facebook.com/samantha.adeline/">Facebook</a></li>
+       <li> <a  target="_blank "href="https://www.linkedin.com/in/samantha-adeline/">LinkedIn</a></li>
+        <li> <a  target="_blank "href="https://www.instagram.com/samanthaadelinecordova/">Instagram</a></li>
+        </ul>
+      </section>
+      <h3 class="indigo darken-3" class="flow-text">Conheça outras formas de estudar a Bíblia</h3>
+       <nav>
+
+          <ul>
+            <li>
+              <a href="https://biblia.com.br/" target="_blank">
+              Bíblia</a>
+              </li>
+
+            <li>
+              <a href="https://www.novotempo.com/radio/" target="_blank">
+              Radio Novo Tempo</a>
+              </li>
+
+            <li>
+              <a href="https://www.cpb.com.br/" target="_blank">
+              CPB
+              </a>
+              </li>
+
+            <li>
+               <a href="https://www.novotempo.com/tv/" target="_blank">
+              Tv Novo Tempo
+               </a>
+              </li>
+          </ul>
+
+        </nav>
+         <h4 class="flow-text">Quer continuar estudando a palavra de Deus
+     </h4>
+    <div class="cadastro-int">
+				<div class="form-1">
+                    <form action="texts.php" method="post">						
+                        Nome Completo:<br>
+                        <input  type="text" name="Nome" placeholder="Nome Completo" /><br>						
+                        Pais:<br>
+                        <select>
+                            <option>Argentina</option>
+                            <option>Bolivia</option>
+                            <option>Brasil</option>
+                            <option>Chile</option>
+                            <option>Colômbia</option>
+                        </select><br><br>
+                        Telefone:<br>
+                        <input type="number" name="Telefone" /><br>
+                        E-mail:<br>
+                        <input type="text" name="email" />				
+                    </div>	
+                    <div class="form-2">		
+                        Celular:<br>
+                        <input type="number" name="Celular" />						
+                        <div class="botoes">
+                            <input type="submit" name="cadastrar" value="Cadastrar" />
+                            <input type="Reset" value="Limpar" />
+                        </div>	
+                    </form>
+				</div>
+			</div>		
+     <?php
+ $_POST["nome"] . " (email: " . $_POST["email"] . " (tel: " . $_POST["tel"] . ")<br><br>";
+
+?>
+
+
+       <script src="script.js"></script>
+        <script src="materialize.js"></script>
+    </body>
+  </html>
